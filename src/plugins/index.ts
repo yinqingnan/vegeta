ca77cb24e9e6425a45b43252c85b07779ef9373c
yinqingnan <754417575@qq.com>
2021-09-10 09:43:39 +0800

    2021年9月10日09:43:38

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -2,11 +2,12 @@
  * @Descripttion: 
  * @Autor: yqn
  * @Date: 2021-09-03 15:08:07
- * @LastEditTime: 2021-09-09 19:34:57
+ * @LastEditTime: 2021-09-10 09:41:29
  * @FilePath: \src\plugins\index.ts
  * @LastEditors: yqn
  * @doc: 
  */
+
 import { App } from 'vue';
 export { setupNaive } from '../plugins/naive';
 import { hasPermission } from '../directives/permissions';
